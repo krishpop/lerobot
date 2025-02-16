423769dcf3cc4ed5820190205d0a1ad8ac83ab06
Bhavna Sud <bsud2@bohg-ws-5.stanford.edu>
2025-02-16 00:40:16 -0800

    bug fix with tdmpc2 distillation

--- lerobot/scripts/train.py
+++ lerobot/scripts/train.py
@@ -220,8 +220,10 @@ def update_policy_with_critic(
                     obs = torch.cat((normalized_batch["observation.state"], normalized_batch["observation.environment_state"]), dim=-1)
                 else:
                     obs = normalized_batch["observation.state"]
+                obs = obs.permute(1, 0, 2)
+                predicted_action = predicted_action.permute(1, 0, 2)
                 z = critic.model.encode(obs, None)
-                estimated_value = critic.model.Q(z, predicted_action, None, return_type='avg')
+                estimated_value = critic.model.Q(z, predicted_action, None, return_type='min')
                 critic_loss -= estimated_value.mean()
             else:
                 critic_output = critic(batch, predicted_action)
